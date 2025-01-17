b308151c85043aa986d8cc5be1f54b02be0fdbfa
Marek Michalik <marekmichalik43@gmail.com>
2025-01-18 00:33:12 +0100

    Clearing up singleton

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
-import Experience from './Experience/Experience.js'
+import * as THREE from "three";
+import {OrbitControls} from "three/addons/controls/OrbitControls";
+import Resources from "./src/Resources";
+import assets from "./src/assets";
+import World from "./src/World";
 
-new Experience()
+const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(25, window.innerWidth / window.innerHeight, 0.1, 150)
+scene.add(camera)
+
+const renderer = new THREE.WebGLRenderer({antialias: true})
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+document.body.appendChild(renderer.domElement)
+
+const controls = new OrbitControls(camera, renderer.domElement);
+camera.position.set(-20, 20, 20)
+controls.update()
+
+const resources = new Resources(renderer, assets)
+new World(scene, resources)
+
+renderer.setAnimationLoop(() => {renderer.render(scene, camera)})
